d53e63efe61e0f55b52c6a8ff5338a040fa566d9
Ingo Andelhofs <ingoandelhofs@gmail.com>
2020-04-05 19:26:32 +0200

    Changed to GLEW, drawn a triangle and printed the OpenGL version

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
-// Includes
-#define GLFW_INCLUDE_NONE
-
-#include <glad/glad.h>
+#define GLEW_BUILD
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 
 // Constants
-const GLuint WIDTH = 800, HEIGHT = 600;
-
+const GLuint WIDTH = 800;
+const GLuint HEIGHT = 600;
 
 
 /**
@@ -24,33 +22,37 @@ int main() {
         return -1;
     }
 
-    /* Set all the required options for GLFW */
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(WIDTH, HEIGHT, "The Cherno OpenGL", nullptr, nullptr);
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
     if (!window) {
         glfwTerminate();
         std::cout << "Failed to initialize GLFW window" << std::endl;
         return -1;
     }
 
-    /* Initialize glad */
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Initialize GLEW */
+    if(glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
         return -1;
     }
+
+    /* Log the OpenGL version */
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Legacy OpenGL triangle */
+        glBegin(GL_TRIANGLES);
+            glVertex2f(-0.5f, -0.5f);
+            glVertex2f(0.0f, 0.5f);
+            glVertex2f(0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
